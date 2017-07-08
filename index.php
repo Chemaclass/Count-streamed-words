59a4bc7de52b9d4c6a6abbcab22844901268ede3
Chemaclass <chemaclass@outlook.es>
2017-07-08 14:44:45 +0200

    Clean index

--- index.php
+++ index.php
@@ -4,10 +4,25 @@ require __DIR__ . '/vendor/autoload.php';
 use Counter\WordsStreamCounter;
 use Presenter\OrderedPresenter;
 
-$stream = stream_get_contents(fopen("php://stdin", "r"));
-$wordsCounter = new WordsStreamCounter($stream);
+final class Index
+{
+    const PRESENTER_TYPE_JSON = 'json';
 
-echo (new OrderedPresenter($wordsCounter()))();
+    public function __invoke(string $type = '')
+    {
+        $stream = stream_get_contents(fopen("php://stdin", "r"));
+        $wordsCounter = new WordsStreamCounter($stream);
+        return $this->presenterFactory($type, $wordsCounter);
+    }
 
-echo PHP_EOL;
+    private function presenterFactory(string $type, WordsStreamCounter $wordsCounter)
+    {
+        $presenter = new OrderedPresenter($wordsCounter());
+        if (self::PRESENTER_TYPE_JSON === $type) {
+            return (string)($presenter);
+        }
+        return $presenter();
+    }
+}
 
+echo (new Index)($argv[1]??'');
