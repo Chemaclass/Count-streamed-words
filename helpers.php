69326106e7fa57e5b52916fe07c10842de94b6b9
Chemaclass <chemaclass@outlook.es>
2017-07-08 17:44:57 +0200

    check if function exists before declare as global function

--- helpers.php
+++ helpers.php
@@ -1,11 +1,16 @@
 <?php
-function dd()
-{
-    $args = func_get_args();
-    if (1 === count($args)) {
-        var_dump($args[0]);
-    } else {
-        var_dump($args);
+if (!function_exists('dd')) {
+    /**
+     * Just a debug function
+     */
+    function dd()
+    {
+        $args = func_get_args();
+        if (1 === count($args)) {
+            var_dump($args[0]);
+        } else {
+            var_dump($args);
+        }
+        die;
     }
-    die;
 }
